809a3bfd26b75727440337a0e2187d749c894055
Pablo Porto <paulo.assis@ccc.ufcg.edu.br>
2020-11-24 14:50:33 -0300

    first commit with cloned file

--- src/main/java/com/pabloporto/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/pabloporto/workshopmongo/WorkshopmongoApplication.java
@@ -3,6 +3,7 @@ package com.pabloporto.workshopmongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//everything working as we want it to
 @SpringBootApplication
 public class WorkshopmongoApplication {
 
